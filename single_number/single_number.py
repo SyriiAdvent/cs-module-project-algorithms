30ef135e040bcd692af28d7869b9ca286ce24367
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-07-22 14:16:03 -0700

    simple 1 pass solution to single_nums

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,11 +3,12 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    result = 0
+    for i in arr:
+        result ^= i
+    return result
 
-    pass
-
-
+        
 if __name__ == '__main__':
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
